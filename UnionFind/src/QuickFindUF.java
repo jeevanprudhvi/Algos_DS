6579a4452681e6aefd6c399aad77202e51c13892
Jeevan Prudhvi <jeevan.prudhvi@hotmail.com>
2018-08-02 23:17:44 +0530

    Union Find Client Initial Commit

---
+++ UnionFind/src/QuickFindUF.java
@@ -0,0 +1,5 @@
+/**
+ * Created by jstadiko on 2018-08-01.
+ */
+public class QuickFindUF {
+}
---
+++ UnionFind/src/UnionFindClient.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Created by jstadiko on 2018-08-01.
+ */
+public class UnionFindClient {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args){
+        int N = scanner.nextInt();
+        while(true){
+            int p = scanner.nextInt();
+            int q = scanner.nextInt();
+        }
+    }
+}
