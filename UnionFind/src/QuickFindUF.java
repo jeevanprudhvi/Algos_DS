73d3212ee62c3b9cb3daea138fc1bc92b4cf272f
Jeevan Prudhvi <jeevan.prudhvi@hotmail.com>
2018-08-04 14:28:15 +0530

    Quick find implementation

--- UnionFind/src/QuickFindUF.java
+++ UnionFind/src/QuickFindUF.java
@@ -1,7 +1,33 @@
+import java.util.HashMap;
+
 /**
  * Created by jstadiko on 2018-08-01.
  */
 public class QuickFindUF {
+    private int[] a;
+    QuickFindUF(int n){
+        a = new int[n];
+        for(int i=0; i<a.length; i++)
+            a[i] = i;
+    }
+
+    public boolean connected(int p, int q){
+        return a[p] == a[q];
+    }
+
+    public void union(int p, int q){
+        int temp = a[p];
+        for(int i=0; i<a.length; i++){
+            if(a[i]==temp)
+                a[i] = a[q];
+        }
+    }
+
+    public void showConnectedComponents(){
+        for(int i = 0; i < a.length; i++){
+            System.out.print(i+ " - "+ a[i]+ "; ");
+        }
+    }
 }
 
 // 
--- UnionFind/src/UnionFindClient.java
+++ UnionFind/src/UnionFindClient.java
@@ -7,9 +7,20 @@ public class UnionFindClient {
     private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args){
         int N = scanner.nextInt();
+        QuickFindUF uf = new QuickFindUF(N);
         while(true){
             int p = scanner.nextInt();
             int q = scanner.nextInt();
+            if(uf.connected(p,q)){
+                System.out.println(p+" and "+q+" are already connected..!");
+                continue;
+            }
+            else
+                uf.union(p,q);
+
+            System.out.println("==============================");
+            System.out.println("Connected components ::");
+            uf.showConnectedComponents();
         }
     }
 }
