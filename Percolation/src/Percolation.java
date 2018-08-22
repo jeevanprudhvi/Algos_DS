cf1bb53d3d97a41926735062a57d6360ad68ef83
Jeevan Prudhvi <jeevan.prudhvi@hotmail.com>
2018-08-22 12:19:28 +0530

    data type Percolation defined

---
+++ Percolation/src/Percolation.java
@@ -0,0 +1,23 @@
+/**
+ * Created by jstadiko on 2018-08-22.
+ */
+public class Percolation {
+    public Percolation(int n) {
+    }
+    public void open(int row, int col){}    // open site (row, col) if it is not open already
+    public boolean isOpen(int row, int col){
+        return false;
+    }  // is site (row, col) open?
+    public boolean isFull(int row, int col){
+        return false;
+    }  // is site (row, col) full?
+    public int numberOfOpenSites(){
+        return 0;
+    }       // number of open sites
+    public boolean percolates(){
+        return false;
+    }
+    public static void main(String[] args){
+        
+    }
+}
