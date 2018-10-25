befc8685a4c2a2ba858b6680a37a6950af6deeee
Jeevan Prudhvi <jeevan.prudhvi@hotmail.com>
2018-10-25 15:53:22 +0530

    Insertion sort

--- SortUtils/src/SortUtils.java
+++ SortUtils/src/SortUtils.java
@@ -23,14 +23,26 @@ public class SortUtils {
             }
         }
     }
+    public static void insertionSort(Comparable[] elements){
+        for(int i=0; i< elements.length; i++){
+            for(int j=i; j>0; j--){
+                if(less(elements[j],elements[j-1]))
+                    swap(elements, j, j-1);
+                else
+                    break;
+            }
+        }
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String exp = sc.next(); // 12 5 13 2 17 25 22 55 32 27
+        exp = "12 5 13 2 17 25 22 55 32 27";
         String[] elems = exp.split(" ");
         Integer[] items = new Integer[elems.length];
         for(int i=0; i< elems.length; i++)
             items[i] = Integer.parseInt(elems[i]);
-        selectionSort(items);
+//        selectionSort(items);
+        insertionSort(items);
         for(int i=0; i<items.length; i++)
             System.out.print(items[i]+" ");
     }
